a68728f5638ac2f5f93d2a57744152f76e9582c9
bishaldsrija08 <bishaldsrijal1@gmail.com>
2024-09-07 08:58:50 +0545

    DB connectoin

---
+++ app.js
@@ -0,0 +1,18 @@
+//Requires start here
+require("dotenv").config();
+const express = require("express")
+const app = express()
+//Requires end here
+
+//Database connection
+require("./model/index")
+app.get("/", (req, res)=>{
+    res.send("I am live.")
+})
+
+
+
+const PORT =  process.env.PORT || 4000
+app.listen(PORT, ()=>{
+    console.log(`Server has started at port ${PORT}`)
+})
